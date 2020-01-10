36b130ebde8a14f3431ef15dde9e0bb5d065a4e3
Александр Итпеков <itpekov@corp.cintrh.ru>
2020-01-10 12:12:10 +0700

    laravel ui bootstrap auth

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::group(['prefix' => 'admin'], function () {
 });
 
 Route::get('journal', 'JournalController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
